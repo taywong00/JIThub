60b46fa74adb97661f7b3ba2c6ed72f0c5afd410
Taylor Wong <taylorwong@Taylors-MacBook-Pro.local>
2017-01-12 22:13:19 -0500

    created Display.java

---
+++ Display.java
@@ -0,0 +1,24 @@
+public class Display{
+    
+    //Inst Vars
+
+    private String[][] gridDis;
+    private int[][] pointsDis;
+    private int[][] tophersDis;
+
+    private static String[] ALPHABET={
+	"A","B","C","D","E","F","G","H","I","J","K","L","M",
+	"N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    };
+
+    
+    //default constructor
+    public Display(){
+        String[][] gridDis = new String[5][5];
+	int[][] pointsDis = new int[2][5];
+        int[][] tophersDis = new int[2][5];
+    }
+
+
+    public void Populate(String[][] grid){
+	for (String[] i: a)
