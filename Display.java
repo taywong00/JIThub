3af2022ea1dd1efe4eb6958cfe51fc2e208d5a85
Iris Tao <mtao@stuy.edu>
2017-01-15 16:09:56 -0500

    Edited populate() and printGrid()

--- Display.java
+++ Display.java
@@ -7,7 +7,7 @@ public class Display{
     private int[][] tophersDis;
     private int flippedVal;
 
-    private static String[] ALPHABET={
+    private String[] ALPHABET={
 	"A","B","C","D","E","F","G","H","I","J","K","L","M",
 	"N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
     };
@@ -16,47 +16,39 @@ public class Display{
     //default constructor
     public Display(){
         String[][] gridDis = new String[5][5];
-        int alphaIndex = 0;
 	int[][] pointsDis = new int[2][5];
         int[][] tophersDis = new int[2][5];
     }
 
-    //populates gridDis with letters of alphabet
+  //populates gridDis with letters of alphabet
     public void populate(){
 	int alphaIndex = 0;
-	for (String[] r : gridDis) {
-	    for (String c : r) {
-		c = ALPHABET[ alphaIndex ];
-		alphaIndex += 1;
+
+	for (int i = 0; i < gridDis.length; i ++){
+	    for (int u = 0; u < gridDis[1].length; u ++){
+	        gridDis[i][u] = ALPHABET[alphaIndex];
+		alphaIndex ++;
 	    }
 	}
     }
 
-    //prints the grid
+     //prints the grid
     public void printGrid(){
-	for(String[] i : gridDis)
-	    {
+        System.out.println("----------");	
+	for(String[] i :gridDis){
+	 
 		for (String x: i){
-		    System.out.println(x);
+		    System.out.print(x + " ");
 		}
-		System.out.print("\n");	
+		System.out.println("");	
 	    }
+	System.out.println("----------");	
 	
     }
-
-    //copies a meaningful value from class Values into Display to be displayed
-    //simulates a flip
-    public void transcribe(Values grid, int x, int y){
-	String s = "" + Values.getValue(x, y);
-	gridDis[x][y] = s;
-	flippedVal = Values.getValue(x,y);
-    }
-    
-    public void multiplier(){
-        Woo.pointCtr = Woo.pointCtr*flippedVal;
-    }
-    
-
-    
- 
 }
+
+
+
+ 
+
+
