194b64a532864b85bcc799160d0c88c7fc433ca7
iristao <mtao@stuy.edu>
2017-01-17 10:52:26 -0500

    moved instantiations to Woo's main method

--- Woo.java
+++ Woo.java
@@ -2,9 +2,20 @@ public class Woo {
     public static int level = 1;
     public static int pointCtr = 1;
     public static void main(String[] args){
+	/*
 	Display foo = new Display();
 	foo.populate();
 	foo.printGrid();
+	*/
+
+     String[][] gridDis = new String[5][5];
+	int[][] pointsDis = new int[2][5];
+        int[][] tophersDis = new int[2][5];
+    Display obj = new Display();
+    obj.populate(gridDis);
+obj.printGrid(gridDis);
+
+	/*
 	Values boo = new Values();
 	boo.popLineVal(level);
 	boo.popGridVal();
@@ -16,5 +27,6 @@ public class Woo {
 	if (pointCtr == 0){
 	    lose();
 	}
+	*/
     }
 }
