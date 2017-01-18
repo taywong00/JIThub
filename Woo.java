d811e051a81e3da068665c2f61732a89bee4fe63
iristao <mtao@stuy.edu>
2017-01-18 10:57:12 -0500

    fix inst vars issue

--- Woo.java
+++ Woo.java
@@ -1,28 +1,22 @@
 public class Woo {
     public static int level = 1;
-    public static int pointCtr = 1;
+    public int pointCtr = 1;
     public static void main(String[] args){
-
+	
 	//Display
-    String[][] gridDis = new String[5][5];
-    int[][] pointsDis = new int[2][5];
-    int[][] tophersDis = new int[2][5];
-    Display foo = new Display();
-    foo.populate(gridDis);
-    foo.printGrid(gridDis);
+	Display foo = new Display();
+    foo.populate();
+    foo.printGrid();
 
 
     //Value
-	int lastPos = 0;
-	int[] lineVal = new int[25]; //to be transcribed into gridVal
-        int[][] gridVal = new int[5][5]; 
-	int[][] pointsVal = new int[2][5]; //the first row will be the COLUMN
-	int[][] tophersVal = new int[2][5];//the second row will be the ROW
 
-	/*
 	Values boo = new Values();
+	
 	boo.popLineVal(level);
+	
 	boo.popGridVal();
+	/*
 	//turn is played, something is flipped
 	//assume tile is 0,0
 	int x = 0;
