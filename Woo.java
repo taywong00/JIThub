01883b6824688dbd60bc2fbaeffdf406227b6074
iristao <mtao@stuy.edu>
2017-01-18 10:28:56 -0500

    Added Values' inst vars

--- Woo.java
+++ Woo.java
@@ -2,18 +2,22 @@ public class Woo {
     public static int level = 1;
     public static int pointCtr = 1;
     public static void main(String[] args){
-	/*
-	Display foo = new Display();
-	foo.populate();
-	foo.printGrid();
-	*/
 
-     String[][] gridDis = new String[5][5];
-	int[][] pointsDis = new int[2][5];
-        int[][] tophersDis = new int[2][5];
-    Display obj = new Display();
-    obj.populate(gridDis);
-obj.printGrid(gridDis);
+	//Display
+    String[][] gridDis = new String[5][5];
+    int[][] pointsDis = new int[2][5];
+    int[][] tophersDis = new int[2][5];
+    Display foo = new Display();
+    foo.populate(gridDis);
+    foo.printGrid(gridDis);
+
+
+    //Value
+	int lastPos = 0;
+	int[] lineVal = new int[25]; //to be transcribed into gridVal
+        int[][] gridVal = new int[5][5]; 
+	int[][] pointsVal = new int[2][5]; //the first row will be the COLUMN
+	int[][] tophersVal = new int[2][5];//the second row will be the ROW
 
 	/*
 	Values boo = new Values();
