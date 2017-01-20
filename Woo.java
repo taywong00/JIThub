0a507ea5c375c62f952d6635fee5afd81d4aa478
Iris Tao <mtao@stuy.edu>
2017-01-20 11:02:26 -0500

    added input feature

--- Woo.java
+++ Woo.java
@@ -1,30 +1,55 @@
+//import Keyboard;
+
 public class Woo {
     public static int level = 1;
-    public int pointCtr = 1;
+    public static int pointCtr = 1;
+
+    
+    
+    //flip method
+    //precond: have an array
+    public void flip(String letter) {
+    
+    }
+    
     public static void main(String[] args){
-	
-	//Display
-	Display foo = new Display();
+    
+    //Display
+    Display foo = new Display();
     foo.populate();
     foo.printGrid();
 
 
     //Value
 
-	Values boo = new Values();
-	
-	boo.popLineVal(level);
-	
-	boo.popGridVal();
-	/*
-	//turn is played, something is flipped
-	//assume tile is 0,0
-	int x = 0;
-	int y = 0;
-	foo.transcribe(boo, x, y);
-	if (pointCtr == 0){
-	    lose();
-	}
-	*/
+    Values boo = new Values();
+    
+    boo.popLineVal(level);
+    
+    boo.popGridVal();
+
+    System.out.println("Enter a letter coordinate that's shown above");
+    String letter = Keyboard.readString();
+    System.out.println(letter);
+    
+
+    
+    //turn is played, something is flipped
+    //assume tile is 0,0
+    int x = 0;
+    int y = 0;
+    foo.transcribe(boo, x, y);
+    foo.printGrid();
+    if (pointCtr == 0){
+        System.out.println("yuo lose");
+    }
+       
     }
 }
+
+/*
+
+ System.out.print("Guess a num bt " + _lo + " & " + _hi + ": ");
+	    guess = Keyboard.readInt();
+
+*/
